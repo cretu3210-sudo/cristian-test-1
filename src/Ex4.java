cbd75b0e3824f4d09aebc8db8271c0ab83de12a3
Cristian Cretu <cretu.3210@gmail.com>
2026-01-03 20:35:00 +0300

    Add ex4

---
+++ src/Ex4.java
@@ -0,0 +1,13 @@
+public class Ex4 {
+    public static void main(String[] args) {
+
+        int nr1 = 33;
+        int nr2 = 6;
+         if (nr1 > nr2){
+             System.out.println("Primul numar este mai mare ca al doilea.");
+
+        } else {
+             System.out.println("Primul numar este mai mic ca al doilea.");
+         }
+    }
+}
