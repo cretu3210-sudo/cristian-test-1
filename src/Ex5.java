cd9f34fa8da89e4807cf93692780093bc07d8da2
Cristian Cretu <cretu.3210@gmail.com>
2026-01-03 20:41:48 +0300

    Add ex5

---
+++ src/Ex5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Ex5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Input password: ");
+        String password = scan.next();
+        System.out.println("Password was: " + password );
+    }
+}
