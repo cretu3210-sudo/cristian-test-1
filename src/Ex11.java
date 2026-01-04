59ab7aec2877c783af08cbcb0c2d06c62d4f9387
Cristian Cretu <cretu.3210@gmail.com>
2026-01-04 18:54:29 +0300

    Add ex11

---
+++ src/Ex11.java
@@ -0,0 +1,13 @@
+public class Ex11 {
+    public static void main(String[] args) {
+        String text = "Eu locuiesc in satul Horesti.";
+        int i = 0;
+        for (i = 0; i< text.length(); i++){
+
+        }
+        int indiceMaxim = i - 1;
+
+        System.out.println("Numar de caractere: " +i);
+        System.out.println("Indice maxim: " + indiceMaxim);
+    }
+}
